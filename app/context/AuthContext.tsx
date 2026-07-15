b35bfea2e10375bc7119223f20b2fe7ededb661b
emmanuel okuchukwu <emmanuelokuchukwu@emmanuels-MacBook-Pro.local>
2026-07-15 19:38:25 +0100

    code formatting

--- app/context/AuthContext.tsx
+++ app/context/AuthContext.tsx
@@ -9,29 +9,30 @@ import {
 } from 'react';
 import { authService } from '@/app/services/auth';
 
-const AuthContext = createContext<
-  | {
-      userInfo: { username: string; userId: string } | null;
-      onLogin: (email: string, password: string) => void;
-      isLoading: boolean;
-    }
-  | undefined
->(undefined);
+interface IUserInfo {
+  username: string;
+  userId: string;
+}
+
+interface IAuthContext {
+  userInfo: { username: string; userId: string } | null;
+  onLogin: (email: string, password: string) => void;
+  isLoading: boolean;
+}
+
+const AuthContext = createContext<IAuthContext | undefined>(undefined);
 
 const AuthProvider = ({ children }: { children: ReactNode }) => {
-  const [userInfo, setUserInfo] = useState<{
-    username: string;
-    userId: string;
-  } | null>(null);
+  const [userInfo, setUserInfo] = useState<IUserInfo | null>(null);
   const [isLoading, setIsLoading] = useState<boolean>(false);
 
   const onLogin = async (email: string, password: string) => {
     try {
       const response = await authService.onLogin(email, password);
-
       const accessToken = response.userInfo.token;
 
-      const currentUser = authService.extractCurrentUser(accessToken);
+      const currentUser: IUserInfo =
+        authService.extractCurrentUser(accessToken);
       setUserInfo(currentUser);
 
       return response;
