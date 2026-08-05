e443c1158c9370dc5a25b99d3e04b1d4817dbb13
emmanuel okuchukwu <emmanuelokuchukwu@emmanuels-MacBook-Pro.local>
2026-08-05 21:39:54 +0100

    new change

--- app/page.tsx
+++ app/page.tsx
@@ -4,55 +4,46 @@ import { useState, useEffect } from 'react';
 import type { FormEvent, ChangeEvent } from 'react';
 import { useRouter } from 'next/navigation';
 import styled from 'styled-components';
+import * as Yup from 'yup';
+import { Formik } from 'formik';
 import Button from './components/atoms/button';
-import { motion } from 'framer-motion';
-import Carousel from '@/app/components/organisms/Carousel';
-import { useCountDown } from '@/app/hooks';
-import AccountCardPulseEffect from '@/app/components/molecules/pulseEffects/accountCard/AccountCardPulse';
-import { AccountCardLoading } from '@/app/components/molecules/pulseEffects/accountCard';
 import Layout from '@/app/components/template';
 import Input from '@/app/components/atoms/input';
 import { authService } from '@/app/services/auth';
 
-const Orb = styled(motion.div)`
-  pointer-events: none;
-  position: absolute;
-  width: 400px;
-  height: 400px;
-  z-index: 0;
-  border-radius: 50%;
-  transform: translate(-50%, -50%);
-  background: radial-gradient(
-    circle,
-    rgba(212, 168, 83, 0.15) 0%,
-    rgba(10, 10, 10, 0) 70%
-  );
-  //background-color: #6B7280;
-`;
-
-const values = {
+const formValues: { email: string; password: string } = {
   email: '',
   password: '',
 };
 
+const loginSchema = Yup.object().shape({
+  email: Yup.string().email().required(),
+  password: Yup.string()
+    .matches(
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&.#_-])[A-Za-z\d@$!%*?&.#_-]{8,}$/,
+      'Password must contain uppercase, lowercase, number, and special character'
+    )
+    .required(),
+});
+
 export default function Home() {
-  const [loginValues, setLoginValues] = useState<{
-    email: string;
-    password: string;
-  }>(values);
   const [message, setMessage] = useState('');
-  const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
+  // TODO: revisit later
+  const [authenticatedUser, setAuthenticatedUser] = useState<string | null>(
+    () => localStorage.getItem('access_token')
+  );
   const [loading, setLoading] = useState<boolean>(false);
 
-  const onChange = (e: ChangeEvent<HTMLInputElement>) => {
-    setLoginValues({ ...loginValues, [e.target.name]: e.target.value });
-  };
-
-  const handleLogin = async (evt: FormEvent) => {
-    evt.preventDefault();
+  const handleLogin = async ({
+    email,
+    password,
+  }: {
+    email: string;
+    password: string;
+  }) => {
     setLoading(true);
     try {
-      await authService.onLogin(loginValues.email, loginValues.password);
+      await authService.onLogin(email, password);
       setLoading(false);
     } catch (err: any) {
       setLoading(false);
@@ -63,35 +54,62 @@ export default function Home() {
   };
 
   const router = useRouter();
+
+  console.log(authenticatedUser);
+
+  if (authenticatedUser) {
+    router.push('/dashboard');
+  }
   return (
     <div className="main">
       <Layout>
         <h1>Login</h1>
         {message && <span className="message">{message}</span>}
-        <form onSubmit={handleLogin}>
-          <Input
-            name="email"
-            placeholder="John.Doe@eyefind.com"
-            type="text"
-            value={loginValues.email}
-            onChange={onChange}
-          />
-          <Input
-            name="password"
-            placeholder="************************"
-            type="password"
-            value={loginValues.password}
-            onChange={onChange}
-          />
-          <Button
-            variant="outline"
-            fullWidth="full"
-            onClick={() => router.push('/dashboard')}
-            disabled={loading}
-          >
-            {loading ? 'Loading...' : 'Login'}
-          </Button>
-        </form>
+        <Formik
+          initialValues={formValues}
+          onSubmit={handleLogin}
+          validationSchema={loginSchema}
+        >
+          {({
+            handleSubmit,
+            handleChange,
+            handleBlur,
+            errors,
+            touched,
+            values,
+          }) => (
+            <form>
+              <Input
+                name="email"
+                placeholder="John.Doe@eyefind.com"
+                type="text"
+                value={values.email}
+                onBlur={handleBlur('email')}
+                onChange={handleChange('email')}
+              />
+              {touched && errors.email}
+              <Input
+                name="password"
+                placeholder="************************"
+                type="password"
+                value={values.password}
+                onBlur={handleBlur('password')}
+                onChange={handleChange('password')}
+              />
+              {touched && errors.password}
+              <Button
+                variant="outline"
+                fullWidth="full"
+                onClick={() => router.push('/dashboard')}
+                disabled={loading}
+                type="submit"
+                onClick={() => handleSubmit(values)}
+              >
+                {loading ? 'Loading...' : 'Login'}
+              </Button>
+            </form>
+          )}
+        </Formik>
       </Layout>
     </div>
   );
